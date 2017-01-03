9dc2df5b49138d53cdb1938da2b390073204515a
BruceleeThanh <nguyentrungthanh602@gmail.com>
2017-01-03 16:08:39 +0700

    create viewed and clicked noti

--- routes/notification.js
+++ routes/notification.js
@@ -95,4 +95,158 @@ module.exports = function (app, redisClient) {
             }
         });
     });
+
+    app.post('/api/notification/viewed', function (req, res) {
+        var data = {};
+        var fields = [{
+            name: 'token',
+            type: 'string',
+            required: true
+        }, {
+            name: 'list_id_notification',
+            type: 'strings_array',
+            required: true
+        }];
+        var currentUser = null;
+        var list_id_notification = [];
+        async.series({
+            validate: function (callback) {
+                validator(req.body, fields, function (error, result) {
+                    if (error) {
+                        return callback(error, null);
+                    } else {
+                        data = result;
+                        if (data.list_id_notification) {
+                            list_id_notification = JSON.parse(JSON.stringify(data.list_id_notification));
+                        }
+                        return callback(null, null);
+                    }
+                });
+            },
+            getLoggedin: function (callback) {
+                authentication.getLoggedin(redisClient, data.token, function (error, result) {
+                    if (error) {
+                        return callback(-1, null);
+                    } else if (!result) {
+                        return callback(-3, null);
+                    } else {
+                        currentUser = JSON.parse(result);
+                        return callback(null, null);
+                    }
+                });
+            },
+            viewed: function (callback) {
+                var leng = list_id_notification.length;
+                for (let i = 0; i < leng; i++) {
+                    Notification.findByIdAndUpdate(list_id_notification[i], {viewed: 1}, {new: true}, function (error, result) {
+
+                    });
+                    if (i == leng - 1) {
+                        return callback(null, null);
+                    }
+                }
+            }
+        }, function (error, result) {
+            if (error) {
+                var code = error;
+                var message = '';
+                if (error === -1) {
+                    message = 'Redis error';
+                } else if (error === -2) {
+                    message = 'DB error';
+                } else if (error === -3) {
+                    message = 'Token is not found';
+                } else {
+                    message = error;
+                    code = 0;
+                }
+                res.json({
+                    code: code,
+                    message: message
+                });
+            }
+            else {
+                res.json({
+                    code: 1,
+                    data: 'Done'
+                });
+            }
+        });
+    });
+
+    app.post('/api/notification/clicked', function (req, res) {
+        var data = {};
+        var fields = [{
+            name: 'token',
+            type: 'string',
+            required: true
+        }, {
+            name: 'id_notification',
+            type: 'string',
+            required: true
+        }];
+        var currentUser = null;
+        async.series({
+            validate: function (callback) {
+                validator(req.body, fields, function (error, result) {
+                    if (error) {
+                        return callback(error, null);
+                    } else {
+                        data = result;
+                        return callback(null, null);
+                    }
+                });
+            },
+            getLoggedin: function (callback) {
+                authentication.getLoggedin(redisClient, data.token, function (error, result) {
+                    if (error) {
+                        return callback(-1, null);
+                    } else if (!result) {
+                        return callback(-3, null);
+                    } else {
+                        currentUser = JSON.parse(result);
+                        return callback(null, null);
+                    }
+                });
+            },
+            clicked: function (callback) {
+                Notification.findByIdAndUpdate(data.id_notification, {clicked: 1}, {new: true}, function (error, result) {
+                    if (error) {
+                        require(path.join(__dirname, '../', 'ultis/logger.js'))().log('error', JSON.stringify(error));
+                        if (typeof callback === 'function') return callback(-4, null);
+                    } else {
+                        if (typeof callback === 'function') return callback(null, result);
+                    }
+                });
+            }
+        }, function (error, result) {
+            if (error) {
+                var code = error;
+                var message = '';
+                if (error === -1) {
+                    message = 'Redis error';
+                } else if (error === -2) {
+                    message = 'DB error';
+                } else if (error === -3) {
+                    message = 'Token is not found';
+                } else if(error === -4){
+                    message = 'Can not update this notification';
+                } else {
+                    message = error;
+                    code = 0;
+                }
+                res.json({
+                    code: code,
+                    message: message
+                });
+            }
+            else {
+                var foundNotification = result.clicked;
+                res.json({
+                    code: 1,
+                    data: foundNotification
+                });
+            }
+        });
+    });
 };
